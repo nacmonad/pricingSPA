a06648570978d415239febed9437f0bcaee00638
nacmonad <macnomad84@gmail.com>
2016-05-12 13:50:18 -0400

    developing pricing now

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,17 @@
 var app = angular.module('PricingApp', ['ngAnimate','ngSanitize']);
 
-app.controller('MainCtrl', function ($scope, $element) {
+app.controller('MainCtrl', function ($scope, $element, $timeout) {
 
 	$scope.essayBool = true;
 	$scope.presBool = false;
 	$scope.dissBool = false;
+	$scope.heads = ["What is your <br> deadline?", "High School", "College", "University", "Master's","PhD"];
+	$scope.dateNow = new Date();
+	$scope.selectedDeadline;
+	$scope.difficultyLevel;
+	$scope.mappedDeadline;
 
+	//menu control
 	$scope.showEssay = function () {
 		$scope.presBool = false;
 		$scope.dissBool = false;
@@ -27,53 +33,91 @@ app.controller('MainCtrl', function ($scope, $element) {
 		console.log("diss Bool now true")
 
 	}
+
+	$scope.mapDeadline = function () {
+		console.log($scope.dateNow);
+		$scope.mappedDeadline = ["Computed DL1", "Computed D2", "Computed DL3","Computed DL4","Computed DL5","Computed DL6","Computed DL7","Computed DL8","Computed DL9" ];
+	}
+	$scope.mapDeadline();
+
+
+
+	//table cell listen events
+	
+		$scope.$on('cell-activated', function (event, args) {
+					
+					$scope.selectedDeadline = args.deadline;
+					$scope.difficultyLevel = args.level;
+					$scope.$apply();
+				})
+	
+
 })
 
 .controller('EssayCtrl', function ($scope, $element, $attrs) {
-	$scope.heads = ["When you need <br> it to be done?", "High School", "College", "University", "Master's"];
+	//these can be individually adjusted, although essay/presentation the same
 	$scope.deadlines = [
-			{deadline:15, text:"< 6 hours"},
-			{deadline:30, text:"< 12 hours"},
-			{deadline:50, text:"< 24 hours"},
-			{deadline:100, text:"Within 2 days"},
-			{deadline:150, text:"Within 3 days"},
-			{deadline:200, text:"Within 4 days"},
-			{deadline:300, text:"Within 5 or more days"}];
+			{deadline: 0.25, text:"< 6 hours"},
+			{deadline: 0.5, text:"< 12 hours"},
+			{deadline: 1, text:"< 24 hours"},
+			{deadline:2, text:"Within 2 days"},
+			{deadline:3, text:"Within 3 days"},
+			{deadline:4, text:"Within 4 days"},
+			{deadline:5, text:"Within 5 or more days"}];
 
-			$scope.$on('cell-activated', function (event, args) {
-				console.log("cell " + args.deadline + ", " + args.level);
-
-			});
+			
 })
 .controller('PresCtrl', function ($scope, $element) {
-	$scope.heads = ["When you need <br> it to be done?", "High School", "College", "University", "Master's"];
+	
 	$scope.deadlines = [
-			{deadline:50, text:"< 24 hours"},
-			{deadline:100, text:"Within 2 days"},
-			{deadline:150, text:"Within 3 days"},
-			{deadline:200, text:"Within 4 days"},
-			{deadline:300, text:"Within 5 or more days"}];
+			{deadline: 0.25, text:"< 6 hours"},
+			{deadline: 0.5, text:"< 12 hours"},
+			{deadline: 1, text:"< 24 hours"},
+			{deadline:2, text:"Within 2 days"},
+			{deadline:3, text:"Within 3 days"},
+			{deadline:4, text:"Within 4 days"},
+			{deadline:5, text:"Within 5 or more days"}];
 
-	console.log($element[0]);
+
 })
 .controller('DissCtrl', function ($scope, $element) {
-	$scope.heads = ["When you need <br> it to be done?", "Master's", "PhD"];
+	
 	$scope.deadlines = [
-			{deadline:200, text:"In 3-7 days"},
-			{deadline:300, text:"In 8+ days"},
+			{deadline:6, text:"In 3-7 days"},
+			{deadline:8, text:"In 8+ days"},
 			];
 
 	console.log($element[0]);
 })
+
+.controller('PriceWdgtCtrl', function ($scope, $element) {
+	console.log("derp im the price widget!");
+
+	$scope.pricePerPage = 20.00;
+	$scope.numberOfPages = 1;
+	$scope.numberOfWords = 250;
+	$scope.totalCost = 420.00;
+	$scope.currencyType = "CAD";
+
+	$scope.$watch('numberOfPages', function () {
+		console.log("page change");
+		$scope.numberOfWords = 300 * $scope.numberOfPages;
+	});
+	$scope.$watch('numberOfWords', function () {
+		console.log("wordcnt change");
+		$scope.numberOfPages = $scope.numberOfWords/300;;
+	});
+})
+
 .directive('myCell', function() {
 	return {
 		restrict: 'A',
 		scope: true,
 		controller: ['$scope','$element','$timeout', function ($scope, $element,$timeout) {
-			$timeout(function() {
-				console.log("hello " + $scope.deadline.deadline + " , " + $scope.level);    //angular automatically goes to the parrent scope controller (EssayCtrl, PresCtrl, DissCtrl) to get deadline.deadline
+			//$timeout(function() {
+				//console.log("hello " + $scope.deadline.deadline + " , " + $scope.level);    //angular automatically goes to the parrent scope controller (EssayCtrl, PresCtrl, DissCtrl) to get deadline.deadline
 			
-			});  
+			//});  
 			
 
 		}],
@@ -96,7 +140,7 @@ app.controller('MainCtrl', function ($scope, $element) {
 		scope: true,
 		controller: ['$scope','$element','$timeout', function ($scope, $element,$timeout) {
 			$timeout(function() {
-				console.log("hello popwrap");    //angular automatically goes to the parrent scope controller (EssayCtrl, PresCtrl, DissCtrl) to get deadline.deadline
+				//console.log("hello popwrap");    //angular automatically goes to the parrent scope controller (EssayCtrl, PresCtrl, DissCtrl) to get deadline.deadline
 			
 			});  
 		}],
